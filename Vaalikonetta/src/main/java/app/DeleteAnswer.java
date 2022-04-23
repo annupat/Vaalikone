6c8a61ad6ad14789b724f91bb59a5104077cbb1e
annupat <annukka.patrikainen@student.hamk.fi>
2022-04-23 14:00:06 +0300

    javadoc lisätty

--- Vaalikonetta/src/main/java/app/DeleteAnswer.java
+++ Vaalikonetta/src/main/java/app/DeleteAnswer.java
@@ -14,29 +14,46 @@ import javax.servlet.http.HttpServletResponse;
 import dao.Dao;
 import data.Answer;
 
-@WebServlet(
-    name = "DeleteAnswer",
-    urlPatterns = {"/delete"}
-)
+/**
+ * @author annukkapatrikainen
+ *
+ *         DeleteAnswer poistaa halutun vastuksen
+ *
+ */
+
+@WebServlet(name = "DeleteAnswer", urlPatterns = { "/delete" })
 public class DeleteAnswer extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Luodaan Dao-luokan avulla mysql-yhteys
+	 */
 	private Dao dao;
+
 	public void init() {
-		dao=new Dao("jdbc:mysql://localhost:3306/vaalikone", "admin", "salasana");
+		dao = new Dao("jdbc:mysql://localhost:3306/vaalikone", "admin", "salasana");
 	}
+
+	/**
+	 * Saa kysymys_id:n requestina DeleteAnswer.jsp:lta Saa ehdokas_id:n
+	 * servletContextilla ReadAnwer.java-luokasta Poistaa Dao-luokan metodilla
+	 * halutun ehdokkaan hautun vastauksen Ohjaa muokatun vastauslistan tulostumaan
+	 * poistettu.jsp:lle
+	 */
+
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) 
-	     throws IOException, ServletException {
-		String kysymys_id=request.getParameter("kysymys_id");
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String kysymys_id = request.getParameter("kysymys_id");
 		ServletContext servletcontext = getServletContext();
-		String ehdokas_id=(String) servletcontext.getAttribute("ehdokas_id");
-		ArrayList<Answer> list=null;
+		String ehdokas_id = (String) servletcontext.getAttribute("ehdokas_id");
+		ArrayList<Answer> list = null;
 		if (dao.getConnection()) {
-			list=dao.deleteAnswer(ehdokas_id, kysymys_id);
+			list = dao.deleteAnswer(ehdokas_id, kysymys_id);
 			System.out.println("Kysymys_id: " + kysymys_id);
 			System.out.println("Ehdokas_id: " + ehdokas_id);
-		}	
-		request.setAttribute("answerlist", list);	
-		RequestDispatcher rd=request.getRequestDispatcher("/jsp/poistettu.jsp");  
+		}
+		request.setAttribute("answerlist", list);
+		RequestDispatcher rd = request.getRequestDispatcher("/jsp/poistettu.jsp");
 		rd.forward(request, response);
 	}
 }
