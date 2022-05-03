f70b1118c09f6b203ab6eec954600ff7675664cc
monaelina <mona.jaaskelainen@gmail.com>
2022-05-03 15:51:43 +0300

    kirjautuminen admin etusivulle - TOIMII!! :D

---
+++ Vaalikonetta/src/main/java/filter/AuthFilter.java
@@ -0,0 +1,79 @@
+package filter;
+
+import java.util.Hashtable;
+import java.io.IOException;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.codec.binary.Base64;
+
+@WebFilter(dispatcherTypes = {
+								DispatcherType.REQUEST,
+								DispatcherType.FORWARD,
+								DispatcherType.INCLUDE,
+								DispatcherType.ERROR
+						}
+						, urlPatterns = {"/adminreadquestions"})
+
+public class AuthFilter implements Filter {
+	Hashtable<String, String> ValidUsers = new Hashtable<>();
+	
+	public AuthFilter() {
+}
+	
+		public void destroy() {
+			
+		}
+
+		@Override
+		public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+						throws IOException, ServletException {
+			doFilter((HttpServletRequest)request, (HttpServletResponse)response, chain);
+		}
+		
+		public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+			String auth = request.getHeader("Authorization");
+			
+			if (!allowUser(auth)) {
+				response.setHeader("WWW-Authenticate", "BASIC realm=\"Salainen\"");
+				response.sendError(response.SC_UNAUTHORIZED);
+			} else {
+				chain.doFilter(request, response);
+			}
+			}
+		public void init(FilterConfig fConfig) throws ServletException {
+		}
+		
+		public void getUser() {
+			ValidUsers.put("admin:salasana", "authorized");
+		}
+		
+		protected boolean allowUser(String auth) throws IOException {
+			getUser();
+			if (auth == null) {
+				return false;
+			}
+			if (!auth.toUpperCase().startsWith("BASIC ")) {
+				return false;
+			}
+			String userpassEncoded = auth.substring(6);
+			Base64 base64 = new Base64();
+			String userpassDecoded = new String(base64.decode(userpassEncoded.getBytes()));
+			
+			if ("authorized".contentEquals(ValidUsers.get(userpassDecoded))) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+}
--- Vaalikonetta/src/main/java/rest/AdminReadQuestions.java
+++ Vaalikonetta/src/main/java/rest/AdminReadQuestions.java
@@ -1,40 +1,94 @@
 package rest;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
 import javax.ws.rs.core.MediaType;
 
+
+import com.mysql.jdbc.Statement;
+
 import data.AdminQuestion;
 
-@WebServlet(urlPatterns = {"/adminreadquestions"})
+@WebServlet(
+		name = "AdminReadQuestions",
+		urlPatterns = {"/adminreadquestions"}
+		)
 
-//public class AdminReadQuestions extends HttpServlet {
-//	@Override 
-//	public void doGet(HttpServletRequest request, HttpServletResponse response)
-//		throws IOException {
-	
-public class AdminReadQuestions {
-	@GET
-	@Path("/allquestions")
-	@Produces(MediaType.APPLICATION_JSON)
-	
-	public ArrayList<AdminQuestion> readAllQuestions() {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("vaalikone");
-		EntityManager em = emf.createEntityManager();
-		
-		ArrayList<AdminQuestion> list = (ArrayList<AdminQuestion>) em.createNamedQuery("select a from AdminQuestion a").getResultList();
-		return list;
+public class AdminReadQuestions extends HttpServlet {
+	@Override 
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			
+			RequestDispatcher rd = request.getRequestDispatcher("adminEtusivu.html");
+			rd.include(request,  response);;
+			
+			Connection conn = null;
+			try {
+				Class.forName("com.mysql.jdbc.Driver").newInstance();
+				conn = java.sql.DriverManager.getConnection("jdbc:mysql://localhost:3306/vaalikone", "admin", "salasana");
+				
+				String kysymys = request.getParameter("kysymys");
+				
+				Statement stmt = (Statement) conn.createStatement();
+				
+				readAllAdminQuestions(conn, stmt, kysymys);
+				
+				ResultSet rs = stmt.executeQuery("select * from kysymykset");
+				out.println("<ol>");
+				
+				while (rs.next()) {
+					out.println("<li>"+rs.getString("kysymys"));
+				}
+			} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+			}
+			// out.println("<a href = ./___.html'>Takaisin lomakkeelle</a>");
+			
+			rd = request.getRequestDispatcher("htmlend.html");
+			rd.include(request, response);;
 	}
+	
+	
+	private int readAllAdminQuestions(Connection conn, Statement stmt, String kysymys) throws SQLException {
 		
+		int count = stmt.executeUpdate("select * from kysymykset");
+		return count; 
+	}
 }
+//public class AdminReadQuestions {
+//	@GET
+//	@Path("/allquestions")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	
+//	public ArrayList<AdminQuestion> readAllQuestions() {
+//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("vaalikone");
+//		EntityManager em = emf.createEntityManager();
+//		
+//		ArrayList<AdminQuestion> list = (ArrayList<AdminQuestion>) em.createNamedQuery("select a from AdminQuestion a").getResultList();
+//		return list;
+//	}
+//		
+//}
 
 
 
